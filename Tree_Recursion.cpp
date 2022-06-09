71c83475bcfd7d2862d90f574e2e794be095729f
The-Batman-Code <98730721+The-Batman-Code@users.noreply.github.com>
2022-06-09 23:36:37 +0530

    Add files via upload

---
+++ Tree_Recursion.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+void fun(int n)
+{
+    if(n>0)
+    {
+        cout<<n<<endl;
+        fun(n-1);
+        fun(n-1);
+    }
+}
+int main()
+{
+    fun(3);
+    return 0;
+}
